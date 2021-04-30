2738ba2cfec11f63ea59c04f118b044875e03ff8
Pavel Alina <alina_pavel98@yahoo.com>
2021-05-01 00:01:18 +0300

    sending email with qr code image

--- aplicatie/src/main/java/com/licenta/aplicatie/Service/Email/EmailService.java
+++ aplicatie/src/main/java/com/licenta/aplicatie/Service/Email/EmailService.java
@@ -3,14 +3,19 @@ package com.licenta.aplicatie.Service.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+
 
 @Service
 public class EmailService {
@@ -30,28 +35,64 @@ public class EmailService {
     }
 
     public void sendEmailWithAttachment(String emailAdress) throws MessagingException, IOException {
+//--------------- IT WORKS BUT THE IMEGE SHOULD BE UPLOAD O, NOT LOCAL
+//        MimeMessage message = javaMailSender.createMimeMessage();
+//
+//        // true = multipart message
+//        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//
+//        helper.setTo(emailAdress);
+//
+//        helper.setSubject("Testing from Spring Boot");
+//
+//        // default = text/plain
+//        //helper.setText("Check attachment for image!");
+//
+//        // true = text/html
+////        helper.setText("<h1>Check attachment for image!</h1>", true);
+//        message.setContent
+//                ("<h1>This is a test</h1>"
+//                                + "<img src=\"https://www.linkpicture.com/q/QRCode.png\">",
+//                        "text/html");//        // hardcoded a file path
+//        FileSystemResource file = new FileSystemResource(new File("aplicatie/src/main/resources/QRCodes/QRCode.png"));
+//
+//        helper.addAttachment("QRCode.png", file);
+//
+//        javaMailSender.send(message);
 
-        MimeMessage msg = javaMailSender.createMimeMessage();
+        //--------------- TRYING ANOTHER WAY
 
-        // true = multipart message
-        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
-
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(emailAdress);
-
         helper.setSubject("Testing from Spring Boot");
 
-        // default = text/plain
-        //helper.setText("Check attachment for image!");
+        helper.setSubject("Email with Inline images Example");
+        helper.setText(
+                "<html>"
+                        + "<body>"
+                        + "<div>Dear student,"
+                        + "<div><strong>Add the image to the right:</strong></div>"
+                        + "<div>"
+                        + "<img src='cid:rightSideImage' style='float:right;width:150px;height:150px;'/>"
+                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
+                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
+                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
+                        + "<div>Adding a inline resource/image on to the right of the paragraph.</div>"
+                        + "</div>"
+                        + "<div>Thanks,</div>"
+                        + "kalliphant"
+                        + "</div></body>"
+                        + "</html>", true);
+        helper.addInline("rightSideImage",
+                new File("D:/LICENTA/BACKEND/LICENTA_BACKEND/aplicatie/src/main/resources/QRCodes/QRCode.png"));
 
-        // true = text/html
-        helper.setText("<h1>Check attachment for image!</h1>", true);
-
-        // hardcoded a file path
-        FileSystemResource file = new FileSystemResource(new File("aplicatie/src/main/resources/QRCodes/QRCode.png"));
-
-        helper.addAttachment("QRCode.png", file);
-
-        javaMailSender.send(msg);
+        javaMailSender.send(message);
 
     }
+
+    String generateContentId(String prefix) {
+        return String.format("%s-%s", prefix, UUID.randomUUID());
+    }
 }
+
