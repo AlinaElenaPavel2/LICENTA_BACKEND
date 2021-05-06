4e4257ff3bdef56388fe1f15030c4977753898d1
Pavel Alina <alina_pavel98@yahoo.com>
2021-05-06 15:27:43 +0300

    am adaugat endponituri pentru persistarea imaginilor de profil ale userilor

--- aplicatie/src/main/java/com/licenta/aplicatie/Controller/FileStorage/FileStorageController.java
+++ aplicatie/src/main/java/com/licenta/aplicatie/Controller/FileStorage/FileStorageController.java
@@ -2,28 +2,39 @@ package com.licenta.aplicatie.Controller.FileStorage;
 
 import com.licenta.aplicatie.Service.FileStorage.FileStorageService;
 import com.licenta.aplicatie.Service.FileStorage.UploadFileResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 
 @RestController
 @RequestMapping("api/licenta/fileStorage")
 public class FileStorageController {
-    private static final Logger logger = LoggerFactory.getLogger(FileStorageController.class);
+//    private static final Logger logger = LoggerFactory.getLogger(FileStorageController.class);
     @Autowired
     private FileStorageService fileStorageService;
 
@@ -61,7 +72,8 @@ public class FileStorageController {
         try {
             contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
         } catch (IOException ex) {
-            logger.info("Could not determine file type.");
+//            logger.info("Could not determine file type.");
+            System.out.println("Could not determine file type.");
         }
 
         // Fallback to the default content type if type could not be determined
@@ -74,4 +86,44 @@ public class FileStorageController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
+
+    @CrossOrigin
+    @PostMapping("/uploadProfilePicture/user/id={id}")
+    public ResponseEntity<?> uploadPicture(@PathVariable("id") int id,@RequestBody String imgBase64) throws Exception {
+       try {
+           String partSeparator = ",";
+           if (imgBase64.contains(partSeparator)) {
+               String encodedImg = imgBase64.split(partSeparator)[1];
+               byte[] decodedImg = Base64.getDecoder().decode(encodedImg.getBytes(StandardCharsets.UTF_8));
+               ByteArrayInputStream bis = new ByteArrayInputStream(decodedImg);
+               BufferedImage bImage2 = ImageIO.read(bis);
+               ImageIO.write(bImage2, "png", new File("aplicatie/src/main/resources/ProfilePictures/user_"+id+".png") );
+               System.out.println("image created");
+           }
+           return new ResponseEntity<>("image created", HttpStatus.OK);
+       }catch (Exception ex)
+       {
+           return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+       }
+    }
+
+
+    @CrossOrigin
+    @RequestMapping(value = "/profilePicture/user/id={id}", method = {RequestMethod.GET})
+    public ResponseEntity<?> getProfilePicture(@PathVariable("id") int id) throws Exception {
+        try {
+
+            String imagePath="aplicatie/src/main/resources/ProfilePictures/user_"+id+".png";
+            File file = new File(imagePath);
+            FileInputStream imageInFile = new FileInputStream(file);
+            byte imageData[] = new byte[(int) file.length()];
+            imageInFile.read(imageData);
+            String base64Image = Base64.getEncoder().encodeToString(imageData);
+            return new ResponseEntity<>(base64Image, HttpStatus.OK);
+        }catch (Exception ex)
+        {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
